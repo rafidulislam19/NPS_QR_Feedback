4808a9ec0771b8f8864f2a63c392dd7f0afdb154
Rafidul Islam <68073987+rafidulislam19@users.noreply.github.com>
2024-08-15 12:07:24 +0600

    access_management/

--- apps.py
+++ apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
 
-class LogConfig(AppConfig):
+
+class AccessManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'log'
-    verbose_name = _("System Logs")
-
-    def ready(self):
-        import log.signals
+    name = 'access_management'
